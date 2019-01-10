205dd3107f4e6ec7d62f820ff21d54ddd35d63f3
ZalewaCan <43707897+ZalewaCan@users.noreply.github.com>
2019-01-09 19:38:52 -0700

    Add files via upload

--- first_script.py
+++ first_script.py
@@ -1,17 +1,17 @@
 Gstart = int(input('Input start gauge '))
 Gend = int(input('Input final gauge '))
 Tm = Gstart - Gend
-	print('You loaded',Tm,'m3')
+print('You loaded',Tm,'m3')
 b = Tm
 
-	if (b < 24 ):
-		print('This is small load')
-	elif (b < 0):
-		print('Start gauge cant be smaller than end gauge')
-	elif (b > 24 and b < 30):
-		print('This is big load')
-	else:
-		print('You are overloaded')
+if (b < 24 ):
+        print('This is small load')
+elif (b < 0):
+	print('Start gauge cant be smaller than end gauge')
+elif (b > 24 and b < 30):
+	print('This is big load')
+else:
+	print('You are overloaded')
 	
 
 
